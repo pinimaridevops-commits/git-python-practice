12641f9e69d6343e4743a7651047e2ae3474d88e
pinidevops <pinimaridevops@gmail.com>
2026-01-17 21:19:48 +0200

    Add current time to greeting message

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-def greet(name):
-    return f"Hello, {name}!"
+from datetime import datetime
 
+def greet(name):
+    current_time = datetime.now().strftime("%H:%M")
+    return f"Hello, {name}! Current time is {current_time}"
 
 if __name__ == "__main__":
     user = "World"
